6532f6b41536d0c8cc80d7cee4532be1ca7b69d0
kit0-0 <kt1767@gmail.com>
2023-12-22 00:19:07 +0530

    Add Welcome page component

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,24 @@
+// src/pages/Welcome.jsx
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Welcome() {
+  const history = useNavigate();
+
+  const handleSignOut = () => {
+    history("/");
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Welcome to the Dashboard</h2>
+      <p>You have successfully logged in.</p>
+      <Button variant="primary" onClick={handleSignOut}>
+        Sign Out
+      </Button>
+    </div>
+  );
+}
+
+export default Welcome;
